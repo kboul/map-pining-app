6365fb783141b8fa93df5d7ab69256eafa9a0c9e
kboul <kos.voulgaridis@gmail.com>
2021-10-31 22:59:21 +0200

    Connect mongodb, add prettierrc

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,17 @@
 import express, { Application, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
+
+const mongoUrl: string = process.env.MONGO_URL as string;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("Mongo DB connected"))
+  .catch((error) => console.log(error));
 
 app.get("/", (_, res: Response) => {
   res.send("Hello");
