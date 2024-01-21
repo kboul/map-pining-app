82a64d2edc0fe3b33194e77a12fcf760e530a2e4
kboul <kos.voulgaridis@gmail.com>
2024-01-21 16:57:31 +0200

    Fix typescript error on users route

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -34,18 +34,19 @@ appRouter.post("/login", async (req: Request, res: Response) => {
   try {
     // find user
     const user = await User.findOne({ username: req.body.username });
-    if (!user)
-      res.status(400).json({ message: "This user does not seem to exist." });
+    if (!user) res.status(400).json({ message: "This user does not seem to exist." });
 
-    // validate password
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    !validPassword &&
-      res.status(400).json({ message: "Wrong username or password" });
+      if(user) {
+        // validate password
+        const validPassword = await bcrypt.compare(
+          req.body.password,
+          user.password
+        );
+        !validPassword &&
+          res.status(400).json({ message: "Wrong username or password" });
 
-    res.status(200).send({ _id: user._id, username: user.username });
+        res.status(200).send({ _id: user._id, username: user.username });
+      }
   } catch (error) {
     res
       .status(500)
